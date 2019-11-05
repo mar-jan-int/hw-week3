b304a2e6ebdc8e13ed063fa9ecfd2cd3cc42d0d0
macki-int <macki@int.pl>
2019-11-05 16:04:35 +0100

    modyfi nbp client

--- src/main/java/pl.akademiaspring/hwweek/client/CurrencyNBPClient.java
+++ src/main/java/pl.akademiaspring/hwweek/client/CurrencyNBPClient.java
@@ -6,7 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 import pl.akademiaspring.hwweek.model.Currency;
+import pl.akademiaspring.hwweek.model.Rate;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Controller
@@ -27,7 +31,11 @@ public class CurrencyNBPClient {
                 HttpMethod.GET,
                 httpEntity.EMPTY,
                 Currency[].class);
-
-        System.out.println(exchange.toString());
+        List<Rate> rates = new ArrayList<>();
+        rates = exchange.getBody()[0].getRates();
+        for (Rate r: rates
+             ) {
+            System.out.println(r.getCurrency());
+        }
     }
 }
